7e39c5e35667740ba752fc3f7f144a35d4bc07a0
Muzeef <ansarimuzeef@gmail.com>
2022-07-18 14:00:29 +0500

    Counter Projects

---
+++ index.js
@@ -0,0 +1,37 @@
+var counter = document.querySelector("#counter")
+var buttons = document.querySelector('.btn')
+let count = 0;
+
+function changeCounter(buttons) {
+    if(buttons == plus){
+        count ++;
+    }else if(buttons == minus){
+        count --;
+    }else if (buttons == reset){
+        count = 0;
+    }
+    if (count > 0){
+        counter.style.color = 'green';
+    }if(count < 0){
+        counter.style.color = 'red';
+    }
+    counter.innerHTML = count;
+}
+
+// function changeCounter(buttons) {
+//     if(buttons == plus){
+//     count ++;
+//     }
+//     else if (buttons == minus){
+//         count -- ;
+//     }else if (buttons == reset){
+//         count = 0;
+//     }
+//     if (count > 0){
+//         counter.style.color = 'green'
+//     } 
+//     if(count < 0){
+//         counter.style.color = 'red'
+//     }
+//     counter.innerHTML = count
+// }
